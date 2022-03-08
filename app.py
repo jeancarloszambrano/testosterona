1a54d45d6728f2099327becfbe2d7aca4031e249
Jean Zambrano <72208463+jeancarloszambrano@users.noreply.github.com>
2022-03-08 20:59:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ st.sidebar.subheader('UROS')
 pagina = st.sidebar.selectbox('Menu', Menu)
 st.sidebar.subheader('Grupo de pesquisa em Urológia')
 st.sidebar.subheader('Universidade Estadual de Feira de Santana')
+st.sidebar.subheader('Monique Tonani Novais')
+st.sidebar.subheader('Anna Paloma Martins Rocha Ribeiro')
+st.sidebar.subheader('Caroline Santos Silva')
+st.sidebar.subheader('Jean Carlos Zambrano Contreras')
+st.sidebar.subheader('José de Bessa Júnior')
 
 if pagina == 'Home':
 	from PIL import Image
