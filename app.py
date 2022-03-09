cb5b2aae7be0ac91f5343f517f7afaf28333efb6
Jean Zambrano <72208463+jeancarloszambrano@users.noreply.github.com>
2022-03-09 13:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -95,8 +95,10 @@ if pagina == 'Modelo':
 		pred = float(predict_model(modelo1, data = dados)['Label'].round(0))
 		saida = 'O valor predito é de {:.0f}'.format(pred)
 		st.subheader(saida)
-		if pred == 0: st.subheader("Predição de Diagnóstico: **NEGATIVO PARA DEFICIENCIA DE TESTOTERONA**)
-		if pred == 1: st.subheader("Predição de Diagnóstico: **POSITIVO PARA DEFICIENCIA DE TESTOTERONA**)
+		if pred < 0: 
+			st.subheader("Predição de Diagnóstico: **NEGATIVO PARA DEFICIENCIA DE TESTOTERONA**)
+		else
+			st.subheader("Predição de Diagnóstico: **POSITIVO PARA DEFICIENCIA DE TESTOTERONA**)
 					  
 		
 		
