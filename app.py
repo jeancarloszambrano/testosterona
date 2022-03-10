0d684eac0af90337af6425c52395153cb54968a7
Jean Zambrano <72208463+jeancarloszambrano@users.noreply.github.com>
2022-03-09 23:03:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from pycaret.classification import load_model, predict_model
 
 
-Menu = ['Home', 'Modelo', 'Caracteristicas']
+Menu = ['Home', 'Modelo', 'Modelagem']
 
 
 st.sidebar.subheader('UROS')
@@ -36,10 +36,11 @@ if pagina == 'Home':
 		 'de predição clínica combinam vários preditores com base nos pesos atribuídos a cada preditor, obtendo '+
 		 'um risco ou probabilidade. A probabilidade de ter ou não a doença, que pode ser usada para solicitar '+
 		 'encaminhamento urológico para testes adicionais com base no risco de uma condição de saúde particular'+
-		 '(NOVAES et al., 2021).'+
-		 'Pensando na democratização da ciência e retorno dos resultados das pesquisas, no grupo de pesquisa'+
+		 '(NOVAES et al., 2021).')
+	
+	st.write('Pensando na democratização da ciência e retorno dos resultados das pesquisas, no grupo de pesquisa'+
 		 'em Urologia (Uros) da Universidade Estadual de Feira de Santana, desenvolvemos um aplicativo que permite'+
-		 'prever a deficiência de testosterona em homens entre 45 – 85 anos utilizando técnicas de Machine Learning (ML)')
+		 'prever a deficiência de testosterona em homens utilizando parâmetros simples com técnicas de Machine Learning (ML)')
 	
 	st.write('**Referencias**')
 	
@@ -49,6 +50,7 @@ if pagina == 'Home':
 		 '<https://linkinghub.elsevier.com/retrieve/pii/S2352914821000289>. Acesso em: 4 mar. 2022.')
 
 if pagina == 'Modelo':
+	
 	from PIL import Image
 	image = Image.open('logo.jpg')
 	st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="JPEG")
@@ -70,9 +72,6 @@ if pagina == 'Modelo':
 	def get_data():
     		return pd.read_csv('testost2.csv')
 
-	
-		
-	
 	Idade = st.number_input('Idade', min_value=21, max_value=97, step=1)
 	GLI = st.number_input("Glicemia: (em mg/dl)", min_value=10, max_value=384)
 	TGL = st.number_input("Triglicerídeos: (em mg/dl)", min_value=1, max_value=980)
@@ -100,23 +99,83 @@ if pagina == 'Modelo':
 			
 		
 		
-if pagina == 'Caracteristicas':
+if pagina == 'Modelagem':
 	from PIL import Image
 	image = Image.open('logo.jpg')
 	st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="JPEG")
+	st.markdown('---')
 	
-	st.write('**Fig 1. AUC**')
+	st.title('MODELAGEM DOS DADOS')
+	
+	st.write('Aprendizado de Máquina (ou Machine Learning) é uma das técnicas utilizadas'+
+		 'na Inteligência Artificial que usa algoritmos baseados em matemática e'+
+		 'algoritmos com a finalidade de extrair informações de dados brutos e representá-los'+
+		 'por meio de algum tipo de modelo matemático e fazer predições a partir de'+
+		 'de extrair informações de dados brutos e representá-los'+
+		 'por meio de algum tipo de modelo matemático e fazer'+
+		 'novos conjuntos de dados.')
+		 
+	st.write('Para a modelagem dos dados, foram coletadas informações sob idade, glicemia'+
+		 'em jejum (mg/dl), triglicerídeos (mg/dl), colesterol total (mg/dl), HDL (mg/dl) e'+
+		 'LDL (mg/dl), circunferência de cintura (cm), hipertensão arterial (Sim - Não) e'+
+		 'nível de testosterona de 4258 homens com idade entre 20-90 anos da Região'+
+		 'Metropolitana de Feira de Santana.')
+	
+	st.write('Nós, testamos diferentes algoritmos de Aprendizado de Máquina para prever a'+
+		 'deficiência de testosterona (testosterona <300 ng/dl) com algoritmos de'+
+		 'aprendizado supervisionado do tipo classificação. A proporção de homens com'+
+		 'deficiência de testosterona na amostra foi de 23,5%, pelo que aplicamos técnicas'+
+		 'oversampling para lidar com os dados desequilibrados (técnica SMOTE).')
+	
+	st.write('Dividimos a amostra em amostra de teste (1278 casos) e amostra de treino'+
+		 '(2980 casos), rodamos diferentes algoritmos, e selecionamos os algoritmos que'+
+		 'apresentaram o maior Recall (Sensibilidade), situação em que os Falsos'+
+		 'Negativos são considerados mais prejudiciais que os Falsos Positivos. Os'+
+		 'resultados são apresentados na tabela 1.')
+	
+	st.write('**Tabela 1. Algoritmos de aprendizado de maquina.**)
+	from PIL import Image	 
+	tabela1 = Image.open('tabela1.png')
+	st.image(tabela1, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="png")
+		 
+	st.write('Treinamos os modelos utilizando o método de validação cruzada (k-fold = 10),'+
+		 'otimizamos o recall com 1000 interações. A regressão logística mostrou o'+
+		 'melhor desempenho no recall e representamos graficamente AUC (figura 1). O'+
+		 'AUC representa o grau ou medida de discriminação, quanto maior a AUC'+
+		 'melhor o modelo, o seja distingue melhor entre pacientes com deficiência de'+
+		 'testosterona e pacientes sem deficiência (AUC = 0.79).')
+		 
+	st.write('**Figura 1. AUC regressão logística**')
 	from PIL import Image
 	AUC = Image.open('AUC.png')
 	st.image(AUC, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="png")
 	
+	st.write('Outra métrica bastante utilizada para problemas de classificação é a matriz de'+
+		 'confusão, que oferece um detalhamento do desempenho do modelo de'+
+		 'classificação, mostrando, para cada classe, o número de classificações corretas'+
+		 'em relação ao número de classificações preditas pelo modelo. A matriz de'+
+		 'confusão pode ser usada para calcular outras métricas, tais como o número de'+
+		 'Falsos Positivos (quando o resultado esperado é negativo, mas o modelo'+
+		 'resulta em positivo), Falsos Negativos (quando o resultado esperado é positivo,'+
+		 'mas o modelo resulta em negativo), Verdadeiros Positivos (quando o resultado'+
+		 'esperado é positivo e o modelo resulta em positivo) e Verdadeiros Negativos'+
+		 '(quando o resultado esperado é negativo e o modelo resulta em negativo).')
+		 
+	st.write('**Tabela 2. Matriz de confusão**')]
+	Matrix = Image.open('MatrixConf.png')
+	st.image(Matrix, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="png")
 	
+	st.write('Sensibilidade é a proporção de participantes que têm a doença e o modelo os'+
+		 'classificou com deficiência de testosterona. Nosso modelo apresenta uma'+
+		 'sensibilidade de 92,9% e especificidade de 35,8% isto porque selecionamos e'+
+		 'otimizamos algoritmos com o maior Recall.')
+	 		 
 	st.write('**Fig 2. Recall**')
 	from PIL import Image
 	Recall = Image.open('recall.png')
 	st.image(Recall, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="png")
 	
-	st.write('**Fig 3. Matriz de confusão**')
-	Matrix = Image.open('MatrixConf.png')
-	st.image(Matrix, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="png")
+	st.write('**Referencias**')
 	
+	st.write('')
+		
