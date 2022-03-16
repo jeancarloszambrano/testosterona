c22ce1fbf4320b936b14a029bc2ca119475d378e
Jean Zambrano <72208463+jeancarloszambrano@users.noreply.github.com>
2022-03-16 16:49:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if pagina == 'Modelo':
 		st.subheader('NEGATIVO PARA DEFICIÊNCIA DE TESTOSTERONA') if saida == '0' else st.subheader('POSITIVO PARA DEFICIÊNCIA DE TESTOSTERONA')
 		st.write('Glicemia baixa ou hipoglicemia.') 
 		if (GLI < 70): 
-		st.write('Glicemia normal.') 
+		st.write('Glicemia normal.')
 		elif (GLI >= 70) and (GLI < 100): 
 		st.write('Glicemia alterada.') 
 		elif (GLI >= 100) and (GLI <= 125): 
